42633c5b7774c3ff67bbadfe3ce4e0645981a487
ya-sh23 <yashpatel231203@gmail.com>
2025-10-05 22:19:20 +0530

    Video Streaming completed. Need to add features.

--- client/src/screens/Room.jsx
+++ client/src/screens/Room.jsx
@@ -1,12 +1,28 @@
-import React, { useEffect, useCallback } from "react";
+import React, { useEffect, useCallback, useState } from "react";
 import { useSocket } from "../context/SocketProvider";
 
 const RoomPage = () => {
   const socket = useSocket();
 
+  const [remoteSocketId, setRemoteSocketId] = useState(null);
+  const [myStream, setMyStream] = useState();
+
   const handleUserJoined = useCallback(({ email, id }) => {
     console.log(`Email ${email} joined the Room...`);
-  });
+    setRemoteSocketId(id);
+  }, []);
+
+  const handleCallUser = useCallback(async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: true,
+      });
+      setMyStream(stream);
+    } catch (err) {
+      console.error("Error accessing media devices:", err);
+    }
+  }, []);
 
   useEffect(() => {
     socket.on("user:joined", handleUserJoined);
@@ -16,8 +32,27 @@ const RoomPage = () => {
   }, [socket, handleUserJoined]);
 
   return (
-    <div>
-      <h1 className="text-4xl font-bold text-white text-center">Lobby</h1>
+    <div className="flex flex-col justify-center items-center min-h-screen gap-2 text-white">
+      <h1 className="text-4xl font-bold">Lobby</h1>
+      <h4>{remoteSocketId ? "Connected" : "No one is in the room"}</h4>
+      {remoteSocketId && (
+        <button
+          className="bg-red-600 px-5 px-3 rounded-lg cursor-pointer"
+          onClick={handleCallUser}
+        >
+          Call
+        </button>
+      )}
+      {myStream && (
+        <video
+          autoPlay
+          muted
+          className="h-[300px] w-[600px] rounded-lg border border-white/30 mt-4"
+          ref={(vid) => {
+            if (vid) vid.srcObject = myStream;
+          }}
+        />
+      )}
     </div>
   );
 };
